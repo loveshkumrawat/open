bf05d7cb1bc1767c5ecdddcca06b61bbc293d77d
Lovesh Kumrawat <kumrawat.lovesh@gmail.com>
2026-02-22 14:54:50 +0530

    22 Feb 2026

---
+++ NoSQL/mongo-queries.sql
@@ -0,0 +1,64 @@
+show dbs;
+
+use test;
+
+db.createCollection("users");
+show collections;
+db.users.insertOne({"name": "Lovesh", "age": 24, 'email': "lk@gmail.com"});
+
+db.users.insertMany([
+    {"name": "Nilesh", "age": 30, 'email': "nd@gmail.com"},
+    {"name": "Swati", "age": 26, 'email': "sx@gmail.com"},
+]);
+
+db.users.find()
+
+db.users.updateOne({"name": "Lovesh"}, {$set:{"age": "45"}});
+
+db.users.updateOne({"name": "Lovesh"}, {$set:{"age": "45", "email": 'asdh'}});
+
+db.users.find()
+
+db.users.insertOne({"name": "Aditya", "age": 42, 'email': "ap@gmail.com"});
+
+db.users.insertOne({"name": "Aditya Pathak", "age": 24, 'email': "TEST@gmail.com"});
+
+db.users.updateOne({"name": "Aditya"}, {$set:{"age": "ghjk", "email": '789f'}});
+
+db.users.updateMany({"name": "Aditya"}, {$set:{"age": "dghj", "email": '586uhrd'}});
+
+db.users.deleteOne({"name": "Aditya"});
+
+db.users.find({"name": "Aditya"});
+
+db.users.updateOne( { "name": "Aditya"}, {$push: {hobbies: "Python"}});
+
+db.users.find({"hobbies": ("Coding", "Python")});
+
+db.users.updateOne( { "name": "Aditya"}, {$pop: {hobbies: 1}});
+
+db.users.updateOne( { "name": "Aditya"}, {$remove: {hobbies: ["gdgrt"]}});
+
+db.users.find();
+
+db.users.updateMany({}, {$set: {"institute": "DSF"}});
+
+db.users.deleteOne({hobbies: "TT"});
+
+db.users.deleteMany({hobbies: "TT"});
+
+db.users.updateMany({age: {$gte: 22}}, { $unset: { age: "" } });
+
+db.users.updateMany(
+  { 
+    $or: [
+      { institute: 'gh' },
+      { age: "dghj" } 
+    ] 
+  },
+  { $set: { institute: "qazxsw" } }
+)
+
+-- db.users.drop();
+
+-- db.dropDatabase();
